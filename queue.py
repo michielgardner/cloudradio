20d6026b86b0543793ab6ed268a186305274f63b
Michiel Gardner <michiel@gardner.nu>
2012-04-19 01:04:00 +0200

    add tracks to beanstalk queue

---
+++ queue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import pickle
+import beanstalkc
+
+b = beanstalkc.Connection()
+data = open('db/playlist.pkl', 'rb')
+playlist = pickle.load(data)
+
+data.close()
+
+for id, (artist, title, twitter) in playlist.iteritems():
+  b.put(pickle.dumps((id, artist, title, twitter)))

864e738484883880d82175658b2a6da5c1181d9b
Michiel Gardner <michiel@gardner.nu>
2012-04-19 00:32:05 +0200

    write playlist to file

--- playlist.py
+++ playlist.py
@@ -2,12 +2,21 @@
 import requests
 import urllib
 import csv
+import pickle
 from config import config
 from cStringIO import StringIO
 
 table = requests.get('https://www.google.com/fusiontables/api/query?sql=' + urllib.quote_plus('SELECT * FROM %s' % config['table']))
 rows = csv.reader(StringIO(table.content))
+playlist = {}
 
-for id, artist, title, twitter in rows:
-  print id, artist, title, twitter
+for row, (id, artist, title, twitter) in enumerate(rows):
+  if row == 0:
+    continue # skip first row
 
+  playlist[id] = (artist, title, twitter)
+
+output = open('db/playlist.pkl', 'wb')
+
+pickle.dump(playlist, output)
+output.close()

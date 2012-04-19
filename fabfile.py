6be88aced17ddc94af7f0e200d926a1d2a9b26ed
Michiel Gardner <michiel@gardner.nu>
2012-04-19 10:17:20 +0200

    move to code folder

--- fabfile.py
+++ fabfile.py
@@ -6,9 +6,9 @@ env.user = config['server_user']
 
 def deploy():
   local("git archive --format=tar HEAD | gzip > deploy.tar.gz")
-  run('rm -rf %s && mkdir -p %s' % (config['server_path'], config['server_path']))
-  put('deploy.tar.gz', '%s/deploy.tar.gz' % config['server_path'])
-  run("cd %s && tar zxf deploy.tar.gz && rm deploy.tar.gz" % config['server_path'])
+  run('rm -rf %s/code && mkdir -p %s/code' % (config['server_path'], config['server_path']))
+  put('deploy.tar.gz', '%s/code/deploy.tar.gz' % config['server_path'])
+  run("cd %s/code && tar zxf deploy.tar.gz && rm deploy.tar.gz" % config['server_path'])
   local('rm deploy.tar.gz')
   restart()
 

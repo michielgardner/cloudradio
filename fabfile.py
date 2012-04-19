6cb43163ac15127a7f5484f5b7cb7d87672bd6d1
Michiel Gardner <michiel@gardner.nu>
2012-04-19 10:23:19 +0200

    copy config at deploy

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,7 @@ def deploy():
   local("git archive --format=tar HEAD | gzip > deploy.tar.gz")
   run('rm -rf %s/code && mkdir -p %s/code' % (config['server_path'], config['server_path']))
   put('deploy.tar.gz', '%s/code/deploy.tar.gz' % config['server_path'])
+  put('config.py', '%s/code/config.py' % config['server_path'])
   run("cd %s/code && tar zxf deploy.tar.gz && rm deploy.tar.gz" % config['server_path'])
   local('rm deploy.tar.gz')
   restart()
